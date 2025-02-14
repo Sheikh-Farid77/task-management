b1928dc341b7d6503936245d133e66acd4566b2f
Sheikh Farid <mdfaridsagor101@gmail.com>
2025-02-14 12:37:01 +0600

    added task modal feature and interactivity

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID,
+    title: "Learn React",
+    description:
+      "I want to learn React in such way that i can treat it like my slave and make it do whatever i want to do",
+    tags: ["web", "React", "js"],
+    priority: "high",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
+  };
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask}></AddTaskModal>}
       <div className="container mx-auto">
         {/* <!-- Search Box --> */}
         <div className="p-2 flex justify-end">
@@ -12,8 +32,8 @@ const TaskBoard = () => {
         </div>
         {/* Search Box Ends  */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction></TaskAction>
-          <TaskList></TaskList>
+          <TaskAction onAddClick={() => setShowAddModal(true)}></TaskAction>
+          <TaskList tasks={tasks}></TaskList>
         </div>
       </div>
     </section>
